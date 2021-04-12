a1ef20e999053c81ac1a9ce1eb0d071c006a47a2
Alon Oyler-Yaniv <alon@Alons-MacBook-Pro.local>
2021-04-12 12:59:52 -0400

    update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='oyLabCode',
+    name='oyLabImaging',
     version='0.1.0',
     description='data processing code for the Oyler-Yaniv lab @HMS',
     author='Alon Oyler-Yaniv',
-    url='https://github.com/alonyan/oyLabCode',
+    url='https://github.com/alonyan/oyLabImaging',
     packages=find_packages(include=['oyLabImaging', 'oyLabImaging.*']),
     install_requires=[
         'PyYAML'
