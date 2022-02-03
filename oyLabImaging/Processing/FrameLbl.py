c868d42a239feeeee45171cf9d5faccf8d083fd6
Alon Oyler-Yaniv <alonyan1@gmail.com>
2022-02-03 12:17:48 -0500

    add support for multiple nuclei/cyto channels for segmentation as a simple sum of a list of channels

--- oyLabImaging/Processing/FrameLbl.py
+++ oyLabImaging/Processing/FrameLbl.py
@@ -1,4 +1,4 @@
-# FrameLbl module for microscopy data. 
+# FrameLbl module for microscopy data.
 # Loads, segments, and retrieves single cell data for a single position and a single timepoint
 # AOY
 import pandas as pd
@@ -17,16 +17,16 @@ class FrameLbl(object):
     ----------
     MD : relevant metadata OR
     pth : str path to relevant metadata
-    
+
     Attributes
     ----------
     frame :  int - frame number
     Pos : position name
     acq : acquisition name
     Zindex : Zindex
-    
+
     These must specify a unique frame
-    
+
     Segmentation parameters
     -----------------------
     NucChannel : ['DeepBlue'] str name of nuclear channel
@@ -35,11 +35,11 @@ class FrameLbl(object):
     periring : {[False], True} should it calculate a perinuclear ring?
     periringsize : [5] size of perinuclear ring in pixels
     **kwargs : specific args for segmentation function
-    
+
     Returns
     -------
     FrameLbl instance with segmented cells
-    
+
     Class properties
     ----------------
      'XY',
@@ -65,84 +65,91 @@ class FrameLbl(object):
      'regionprops',
      'weighted_centroid',
      'weighted_centroid_um']
-    
+
     Class methods
     -------------
      'img'
      'scattershow'
-    
+
     """
-    def __init__(self, frame=None, MD=None ,pth=None, Pos=None, acq = None, Zindex=0 ,register=True ,periring=False, periringsize=5, NucChannel='DeepBlue',cytoplasm=False,CytoChannel='Yellow',zernike=False, segment_type='watershed', **kwargs):
+    def __init__(self, frame=None, MD=None ,pth=None, Pos=None, acq = None, Zindex=0 ,register=True ,periring=False, periringsize=5, NucChannel='DeepBlue',cytoplasm=False,CytoChannel=None,zernike=False, segment_type='watershed', **kwargs):
 
         if pth is None and MD is not None:
             pth = MD.base_pth
-            
+
         if any([Pos is None, pth is None, frame is None]):
             raise ValueError('Please input path, position, and frame')
-        
+
         self.pth = pth
-        
+
         if MD is None:
             MD = Metadata(pth)
-        
+
         if MD().empty:
-            raise AssertionError('No metadata found in supplied path') 
-                
+            raise AssertionError('No metadata found in supplied path')
+
         if Pos not in MD.posnames:
-            raise AssertionError('Position does not exist in dataset')  
+            raise AssertionError('Position does not exist in dataset')
         self.posname = Pos
-        
+
         if frame not in MD.frames:
             raise AssertionError('Frame does not exist in dataset')
         self.frame = frame
-        
+
 
         self._seg_fun = segmentation.segtype_to_segfun(segment_type)
-        
-        self.channels = MD.unique('Channel',Position=Pos, frame=frame) 
+
+        self.channels = MD.unique('Channel',Position=Pos, frame=frame)
         self.acq = MD.unique('acq',Position=Pos, frame=frame)
-        
+
         self.XY = MD().at[MD.unique('index',Position=Pos, frame=frame)[0],'XY']
         self._pixelsize = MD()['PixelSize'][0]
 
-        
+
+        NucChannel = NucChannel if isinstance(NucChannel, list) else [NucChannel]
+        CytoChannel = CytoChannel if isinstance(CytoChannel, list) else [CytoChannel]
+
         Data = {};
         for ch in self.channels:
             Data[ch] = np.squeeze(MD.stkread(Channel=ch,frame=frame, Position=Pos, Zindex=Zindex,verbose=False))
-            assert Data[ch].ndim==2, "channel/position/frame/Zindex did not return unique result" 
-        
-        self.imagedims = np.shape(Data[NucChannel]);
+            assert Data[ch].ndim==2, "channel/position/frame/Zindex did not return unique result"
+
+        self.imagedims = np.shape(Data[NucChannel[0]]);
 
         nargs = self._seg_fun.__code__.co_argcount
         args = [self._seg_fun.__code__.co_varnames[i] for i in range(2, nargs)]
         defaults = list(self._seg_fun.__defaults__)
-        input_dict = {args[i]: defaults[i] for i in range(0, nargs-2)} 
-        input_dict = {**input_dict, **kwargs}
-        
+        input_dict = {args[i]: defaults[i] for i in range(0, nargs-2)}
+        input_dict = {**input_dict, **kwargs, 'nucchannel': NucChannel, 'cytochannel': CytoChannel}
+
+
         self._seg_params = input_dict
-        
+
+
         try:
-            imgCyto=Data[CytoChannel]
+            imgCyto=np.sum([Data[ch] for ch in CytoChannel], axis=0)
         except:
             imgCyto=''
-        
-        L = self._seg_fun(img=Data[NucChannel],imgCyto=imgCyto,**kwargs)
-        
 
-            
-        props = measure.regionprops(L,intensity_image=Data[NucChannel])   
-        
-        
-        
+        imgNuc = np.sum([Data[ch] for ch in NucChannel], axis=0)
+
+        L = self._seg_fun(img=imgNuc,imgCyto=imgCyto,**kwargs)
+
+
+
+        props = measure.regionprops(L,intensity_image=Data[NucChannel[0]])
+
+
+
         props_df = regionprops_to_df(props)
         props_df.drop(['mean_intensity', 'max_intensity', 'min_intensity'], axis=1,inplace=True)
         if zernike:
-            
+
             L1 = [list(Zernike.coeff_fast(stats.zscore(r.intensity_image)))[1] for r in props]
-            K1 = [list(Zernike.coeff_fast(stats.zscore(r.intensity_image)))[2] for r in props]  
+            K1 = [list(Zernike.coeff_fast(stats.zscore(r.intensity_image)))[2] for r in props]
             props_df['L'] = L1
             props_df['K'] = K1
-                
+
         for ch in self.channels:
             props_channel = measure.regionprops(L,intensity_image=Data[ch])
             mean_channel = [r.mean_intensity for r in props_channel]
@@ -156,17 +163,17 @@ class FrameLbl(object):
             props_df['min_'+ch] = min_channel
             props_df['90th_'+ch] = Ninty_channel
             props_df['median_'+ch] = median_channel
-            
+
             if zernike:
-                c1 = [list(Zernike.coeff_fast(stats.zscore(r.intensity_image)))[0] for r in props_channel]  
-                props_df['zernike_'+ch] = c1            
-        
-        
+                c1 = [list(Zernike.coeff_fast(stats.zscore(r.intensity_image)))[0] for r in props_channel]
+                props_df['zernike_'+ch] = c1
+
+
         if periring:
             from skimage.morphology import disk, dilation
             se = disk(5)
             Lperi = dilation(L, se)-L
-            
+
             for ch in self.channels:
                 props_channel = measure.regionprops(Lperi,intensity_image=Data[ch])
                 mean_channel = [r.mean_intensity for r in props_channel]
@@ -180,7 +187,7 @@ class FrameLbl(object):
                 props_df['min_'+ch + '_periring'] = min_channel
                 props_df['90th_'+ch + '_periring'] = Ninty_channel
                 props_df['median_'+ch + '_periring'] = median_channel
-        
+
         if cytoplasm:
             pass
 
@@ -194,102 +201,102 @@ class FrameLbl(object):
                 #print('\nRegistered centroids')
             else:
                 print('No drift correction found')
-    
+
         self.regionprops = props_df
-        
-    
+
+
     def __call__(self):
         print('FrameLbl object for position ' + self.posname + ' at frame '+ str(self.frame) + '.')
         print('\nThe path to the experiment is: \n ' + self.pth)
-        print('\n '+ str(self.num) + ' cells segmented using '+ self._seg_fun.__name__)        
+        print('\n '+ str(self.num) + ' cells segmented using '+ self._seg_fun.__name__)
         print('\nAvailable channels are : ' + ', '.join(list(self.channels))+ '.')
-        
-    
-    
+
+
+
     @property
     def num(self):
         return self.regionprops.index.size
-    
+
     @property
-    def centroid(self):     
+    def centroid(self):
         return np.reshape(np.concatenate(self.regionprops['centroid']),(-1,2))
-      
+
     @property
     def weighted_centroid(self):
         return np.reshape(np.concatenate(self.regionprops['weighted_centroid']),(-1,2))
-    
+
     @property
     def area(self):
         return self.regionprops['area']
-    
+
     @property
     def index(self):
         return [i for i,r in enumerate(self.area)]
-    
+
     @property
-    def centroid_um(self):     
+    def centroid_um(self):
         return self.XY + self._pixelsize*self.centroid
-      
+
     @property
     def weighted_centroid_um(self):
         return self.XY + self._pixelsize*self.weighted_centroid
-    
+
     @property
     def area_um2(self):
         return self.area*self._pixelsize**2
 
-    
+
     def mean(self,ch, periring=False):
         assert ch in self.channels, "%s isn't a channel, try %s" % (ch, ', '.join(list(self.channels)))
         if periring:
             return self.regionprops['mean_' + ch + '_periring']
         else:
             return self.regionprops['mean_' + ch]
-        
+
     def median(self,ch, periring=False):
         assert ch in self.channels, "%s isn't a channel, try %s" % (ch, ', '.join(list(self.channels)))
         if periring:
             return self.regionprops['median_' + ch + '_periring']
         else:
             return self.regionprops['median_' + ch]
-    
+
     def minint(self,ch, periring=False):
         assert ch in self.channels, "%s isn't a channel, try %s" % (ch, ', '.join(list(self.channels)))
         if periring:
             return self.regionprops['min_' + ch + '_periring']
         else:
             return self.regionprops['min_' + ch]
-    
+
     def maxint(self,ch, periring=False):
         assert ch in self.channels, "%s isn't a channel, try %s" % (ch, ', '.join(list(self.channels)))
         if periring:
             return self.regionprops['max_' + ch + '_periring']
         else:
             return self.regionprops['max_' + ch]
-    
+
     def ninetyint(self,ch, periring=False):
         assert ch in self.channels, "%s isn't a channel, try %s" % (ch, ', '.join(list(self.channels)))
         if periring:
             return self.regionprops['90th_' + ch + '_periring']
         else:
             return self.regionprops['90th_' + ch]
-    
+
     @property
     def density(self):
         from sklearn.neighbors import NearestNeighbors
         nbrs = NearestNeighbors(n_neighbors=11, algorithm='ball_tree').fit(self.weighted_centroid)
         distances,_   = nbrs.kneighbors(self.weighted_centroid)
         return 1./np.mean(distances[:,1:],axis=1)
-    
-    
-    
+
+
+
     #presentation stuff
     def img(self,Channel='DeepBlue',verbose=False, **kwargs):
         """
         Parameters
         ----------
         Channel : [DeepBlue] str or list of strings
-        
+
         Returns
         -------
         Image at given frame and channels
@@ -298,13 +305,13 @@ class FrameLbl(object):
         pth = self.pth
         MD = Metadata(pth,verbose=verbose)
         return MD.stkread(Channel=Channel,frame=self.frame, Position=self.posname, register=True,verbose=verbose, **kwargs)
-    
+
     def scattershow(self,Channel='DeepBlue'):
         """
         Parameters
         ----------
         Channel : [DeepBlue] str or list of strings
-        
+
         Returns
         -------
         Image at given frame and channels and overlaying points of segmented cells
@@ -313,7 +320,7 @@ class FrameLbl(object):
         img = self.img(Channel=Channel)
         fig, ax = plt.subplots()
         fig.subplots_adjust(bottom=0.1)
-        
+
         #fig = plt.figure(figsize=(12,15))
         #fig.add_axes([0.1,0.6,0.8,0.5])
         plt.gca().set_title('original')
