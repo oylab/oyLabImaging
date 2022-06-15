dbc7cee65f33f7cba54101fa334ff1147d5679dc
Alon Oyler-Yaniv <alonyan1@gmail.com>
2022-06-15 15:57:54 -0400

    improve napari to movie export

--- oyLabImaging/Processing/imvisutils.py
+++ oyLabImaging/Processing/imvisutils.py
@@ -3,32 +3,39 @@ def get_or_create_viewer():
     return napari.current_viewer() or napari.Viewer()
 
 
-def export_napari_to_mp4(filename, fps=8, bitrate=-1, dt=30, timestamp=True):
-    import matplotlib.animation as animation
-    import matplotlib.pyplot as plt
-    #from oyLabImaging.Processing.imvisutils import get_or_create_viewer
-    viewer = get_or_create_viewer() 
-    viewer.scale_bar.unit = "um"
+def export_napari_to_movie(fname, dt=None, fps=8,bitrate=1800,timestamp_position=[10,20],timestamp_color='white'):
+    """
+    Export current napari canvas to a movie.
 
+    Parameters:
+    fname : str - filename, full path including suffix (.mp4/.avi...)
+    dt : numeric, optional - time delta in timelapse experiment
+    fps : numeric - frames per second
+    bitrate : integer
+    timestamp_position : [10,20] tuple/2-list measured from top left corner
+    timestamp_color : str ['white']
+    """
+
+    import matplotlib.animation as animation
     ims=[]
-    fig, ax = plt.subplots(dpi=300, figsize=(12,8))
-    fig.patch.set_facecolor('black')
-    ax.patch.set_facecolor('black')
-    ax.axis('equal')
+    fig, ax = plt.subplots(figsize=np.array(viewer.window.qt_viewer.canvas.size)/100)
+    ax.axis('off')
+    fig.tight_layout()
+    viewer = get_or_create_viewer()
+    viewer.scale_bar.unit = "um"
     for i in range(viewer.dims.range[0][1].astype(int)+1):
         viewer.dims.current_step=[i,0,0]
-        if timestamp:
-            t1 = ax.text(10, 1, "%02d:%02d h"%(int(i*dt/60),i*dt%60),color='w')
-        else:
-            t1 = ax.text(10, 1, "",color='k')
-
-        im = plt.imshow(viewer.screenshot(),animated=True)
+        im = plt.imshow(viewer.window.qt_viewer.canvas.render(),animated=True)
         plt.gca().set_position([0, 0, 1, 1])
-        ims.append([t1, im])
-
+        if dt:
+            t1=plt.text(timestamp_position[0], timestamp_position[1],"%02d:%02d"%(int(i*dt/60),i*dt%60),color=timestamp_color)
+            ims.append([t1, im])
+        else:
+            ims.append([im])
 
     ani=animation.ArtistAnimation(fig,ims, interval=50,blit=True,repeat_delay=1000)
 
-    writer = animation.FFMpegWriter(fps=fps, bitrate=bitrate,codec="h264", extra_args=[])#'-s', '1280x960'
+    writer = animation.FFMpegWriter(fps=fps, bitrate=bitrate,codec="h264")
 
-    ani.save(filename, writer=writer)
+    ani.save(fname, writer=writer)
+    plt.close(fig)
